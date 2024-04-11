e35886e4bd0549bd9ebe7af8f5a735fc97e75748
Jose Saldivar <jose.a.saldivar-1@ou.edu>
2024-04-11 16:06:39 -0500

    push not working

--- datamanipulation.py
+++ datamanipulation.py
@@ -1,5 +1,13 @@
 import csv
 
+# Function to convert height from feet-inches format to inches
+def height_to_inches(height):
+    if '-' in height:
+        feet, inches = height.split('-')
+        return int(feet) * 12 + int(inches)
+    else:
+        return int(height)  # Assuming height is already in inches
+
 # Function to convert player position to number
 def position_to_number(position):
     if position == "QB":
@@ -21,10 +29,12 @@ with open('LR_data.csv', 'r', newline='') as csvfile:
         row = [col if col else '0' for col in row]
         # Convert player position to number
         row[3] = position_to_number(row[3])
+        # Convert height to inches
+        row[4] = height_to_inches(row[4])
         data.append(row)
 
 # Write the updated data to a new CSV file
-with open('LR_data.csv', 'w', newline='') as csvfile:
+with open('newLR_data.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
     writer.writerow(header)  # Write the header
     writer.writerows(data)
