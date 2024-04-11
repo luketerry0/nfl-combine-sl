b664857b2648c2fe82aa76d7d4b60c2cd799c9b5
Luke Terry <luke.terry0682@gmail.com>
2024-04-11 13:44:34 -0500

    combine data

---
+++ preproccessing.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import os
+
+
+# append the CSV files
+dfs = []
+for file in os.listdir("./data"):
+    df = pd.read_csv("./data/" + file)
+    
+    dfs.append(df)
+df_appended = pd.concat(dfs)
+
+# safe final csv file
+df_appended.to_csv("./data/full_data.csv")
+
